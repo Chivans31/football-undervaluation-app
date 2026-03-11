34adde091aa697aee5ab2a94b311fea9cea8f742
Global Name <chinenyevans@gmail.com>
2026-03-11 18:50:50 +0000

    "Update shap and dependencies"

--- src/explainability/shap_explainer.py
+++ src/explainability/shap_explainer.py
@@ -3,7 +3,7 @@ import joblib
 from src.config import CONFIG
 
 reg_model = joblib.load(CONFIG["regression_model_path"])
-explainer = shap.TreeExplainer(reg_model)
+explainer = shap.Explainer(reg_model)
 
 def explain_prediction(full_df):
     shap_values = explainer(full_df)
