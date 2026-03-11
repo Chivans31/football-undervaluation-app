ea86761e1a1b37d704fbed4a08adf2b2a27aebcb
Global Name <chinenyevans@gmail.com>
2026-03-11 18:30:51 +0000

    "Edit shap_explainer"

--- src/explainability/shap_explainer.py
+++ src/explainability/shap_explainer.py
@@ -6,9 +6,5 @@ reg_model = joblib.load(CONFIG["regression_model_path"])
 explainer = shap.TreeExplainer(reg_model)
 
 def explain_prediction(full_df):
-    shap_values = shap.TreeExplainer(reg_model)
-
-    if len(shap_values.shape) ==2:
-        return shap_values[0]
-
+    shap_values = explainer(full_df)
     return shap_values
